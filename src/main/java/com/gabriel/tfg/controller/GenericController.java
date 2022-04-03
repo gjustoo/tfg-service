fe8da7ffdc3b2e18ab15ee2e05eeaaa5bdb4c742
gjustoo <gabrijusto113@gmail.com>
2022-04-03 17:01:37 +0200

    First commit

---
+++ src/main/java/com/gabriel/tfg/controller/GenericController.java
@@ -0,0 +1,7 @@
+package com.gabriel.tfg.controller;
+
+public class GenericController {
+
+    
+    
+}
---
+++ src/test/java/com/gabriel/tfg/TfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.tfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
